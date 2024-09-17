3a1882baacd8420e336616ff37a3d86567f5affe
Forpee <khalilhassam3@gmail.com>
2024-09-17 15:49:39 +0200

    chore: make SNARK to be aggregated public

--- src/spartan/verify_circuit/ipa_prover_poseidon/mod.rs
+++ src/spartan/verify_circuit/ipa_prover_poseidon/mod.rs
@@ -1,6 +1,8 @@
+//! Contains SNARKS that can be aggregated
+
 pub mod batched;
 pub mod batched_ppsnark;
-pub mod ipa_pc;
+pub(crate) mod ipa_pc;
 
 #[cfg(test)]
 mod tests;
--- src/spartan/verify_circuit/mod.rs
+++ src/spartan/verify_circuit/mod.rs
@@ -3,4 +3,4 @@
 pub mod aggregator;
 mod circuit;
 pub(crate) mod gadgets;
-pub(crate) mod ipa_prover_poseidon;
+pub mod ipa_prover_poseidon;
