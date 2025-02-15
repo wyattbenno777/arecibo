6579b355077a6b31278e80457d4d3ef15667996c
Forpee <khalilhassam3@gmail.com>
2025-02-15 11:10:34 +0200

    chore: cleanup spaces

--- src/frontend/r1cs.rs
+++ src/frontend/r1cs.rs
@@ -38,11 +38,8 @@ impl<E: Engine> NovaWitness<E> for SatisfyingAssignment<E> {
   ) -> Result<(R1CSInstance<E>, R1CSWitness<E>), NovaError> {
     let W = R1CSWitness::<E>::new(shape, self.aux_assignment().to_vec())?;
     let X = &self.input_assignment()[1..];
-
     let comm_W = W.commit(ck);
-
     let instance = R1CSInstance::<E>::new(shape, comm_W, X.to_vec())?;
-
     Ok((instance, W))
   }
 }
