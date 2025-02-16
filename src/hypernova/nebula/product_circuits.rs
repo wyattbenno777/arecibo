c22c9eacc7289077eafdb2e2cdeec6f7ddf4e10f
Forpee <khalilhassam3@gmail.com>
2025-02-16 18:17:49 +0200

    feat: nebula ops and scan circuits

--- src/hypernova/nebula/product_circuits.rs
+++ src/hypernova/nebula/product_circuits.rs
@@ -5,11 +5,11 @@ use ff::PrimeField;
 use itertools::Itertools;
 
 use crate::{
-  frontend::{num::AllocatedNum, ConstraintSystem, SynthesisError},
+  frontend::{num::AllocatedNum, ConstraintSystem, PCIndex, SynthesisError},
   gadgets::{
     alloc_one, alloc_zero,
     int::{add, enforce_equal, enforce_lt_32, mul},
-    nebula::{alloc_avt_tuple, countable_hash, randomized_hash_func},
+    nebula::{allocated_avt, countable_hash, randomized_hash_func},
   },
   hypernova::rs::StepCircuit,
 };
@@ -55,10 +55,12 @@ where
     // 2. for i in 0..|RS|
     for (i, (rs, ws)) in self.RS.iter().zip_eq(self.WS.iter()).enumerate() {
       // (a) (a,v,rt) ← RS[i]
-      let (r_addr, r_val, r_ts) = alloc_avt_tuple(cs.namespace(|| format!("rs{i}")), *rs)?;
+      let (r_addr, r_val, r_ts) =
+        allocated_avt(cs.namespace(|| format!("rs{i}")), *rs, PCIndex::ZERO)?;
 
       // (b) (a′,v′,wt) ← WS[i]
-      let (w_addr, w_val, w_ts) = alloc_avt_tuple(cs.namespace(|| format!("ws{i}")), *ws)?;
+      let (w_addr, w_val, w_ts) =
+        allocated_avt(cs.namespace(|| format!("ws{i}")), *ws, PCIndex::ZERO)?;
 
       // (c) gts ← gts + 1
       gts = add(cs.namespace(|| format!("{i},  gts ← gts + 1")), &gts, &one)?;
@@ -141,6 +143,132 @@ impl Default for OpsCircuit {
     OpsCircuit {
       RS: vec![(0, 0, 0); MEMORY_OPS_PER_STEP / 2],
       WS: vec![(0, 0, 0); MEMORY_OPS_PER_STEP / 2],
+    }
+  }
+}
+
+/// Circuit to compute multiset hashes of (IS, FS)
+#[derive(Clone, Debug)]
+pub struct ScanCircuit {
+  IS: Vec<(usize, u64, u64)>, // Vec<(a, v, t)>
+  FS: Vec<(usize, u64, u64)>, // Vec<(a, v, t)>
+}
+
+impl<F> StepCircuit<F> for ScanCircuit
+where
+  F: PrimeField,
+{
+  fn arity(&self) -> usize {
+    5
+  }
+
+  fn synthesize<CS: ConstraintSystem<F>>(
+    &self,
+    cs: &mut CS,
+    z: &[AllocatedNum<F>],
+  ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
+    let (gamma, alpha, mut h_is, mut h_fs, size) = (
+      z[0].clone(),
+      z[1].clone(),
+      z[2].clone(),
+      z[3].clone(),
+      z[4].clone(),
+    );
+
+    // used to assert |IS| = |FS|
+    let mut IS_size_count = alloc_zero(cs.namespace(|| "IS_size_count"));
+    let mut FS_size_count = alloc_zero(cs.namespace(|| "FS_size_count"));
+
+    // 2. for i in 0..|IS|
+    for (i, (is, fs)) in self.IS.iter().zip_eq(self.FS.iter()).enumerate() {
+      // (a) (a,v,it)←IS[i]
+      let (i_addr, i_val, i_ts) =
+        allocated_avt(cs.namespace(|| format!("is{i}")), *is, PCIndex::ZERO)?;
+
+      // (b) (a′,v′,ft) ← FS[i]
+      let (f_addr, f_val, f_ts) =
+        allocated_avt(cs.namespace(|| format!("fs{i}")), *fs, PCIndex::ONE)?;
+
+      // (c) assert a=a′
+      cs.enforce(
+        || "a == a_prime",
+        |lc| lc + i_addr.get_variable(),
+        |lc| lc + CS::one(),
+        |lc| lc + f_addr.get_variable(),
+      );
+
+      // (d) h_IS ← h_IS · Hash(gamma, alpha, a, v, it)
+      //
+      // Get Hash(gamma, alpha, a, v, it)
+      let hash_is = randomized_hash_func(
+        cs.namespace(|| format!("{i}, Hash(gamma, alpha, a, v, it)")),
+        &i_addr,
+        &i_val,
+        &i_ts,
+        &gamma,
+        &alpha,
+      )?;
+
+      // Add to IS size count
+      let IS_counting_el =
+        countable_hash(cs.namespace(|| format!("IS_counting_el_{i}")), &hash_is)?;
+      IS_size_count = add(
+        cs.namespace(|| format!("{i}, IS_size_count ← IS_size_count + IS_counting_el")),
+        &IS_size_count,
+        &IS_counting_el,
+      )?;
+
+      h_is = mul(
+        cs.namespace(|| format!("{i}, update h_is")),
+        &h_is,
+        &hash_is,
+      )?;
+
+      // (e) h_FS ← h_FS · Hash(gamma, alpha, a′, v′, ft)
+      let hash_fs = randomized_hash_func(
+        cs.namespace(|| format!("{i}, Hash(gamma, alpha, fa, fv, ft)")),
+        &f_addr,
+        &f_val,
+        &f_ts,
+        &gamma,
+        &alpha,
+      )?;
+      let FS_counting_el =
+        countable_hash(cs.namespace(|| format!("FS_counting_el_{i}")), &hash_fs)?;
+      FS_size_count = add(
+        cs.namespace(|| format!("{i}, FS_size_count ← FS_size_count + FS_counting_el")),
+        &FS_size_count,
+        &FS_counting_el,
+      )?;
+
+      h_fs = mul(
+        cs.namespace(|| format!("{i}, update h_fs")),
+        &h_fs,
+        &hash_fs,
+      )?;
+    }
+    // assert |IS| = |FS|
+    enforce_equal(cs, || "assert |IS| = |FS|", &IS_size_count, &FS_size_count);
+    enforce_equal(cs, || "|IS| == size", &IS_size_count, &size);
+
+    Ok(vec![gamma, alpha, h_is, h_fs, size])
+  }
+}
+
+impl ScanCircuit {
+  /// Create a new instance of ScanCircuit for computing multiset hashes of
+  /// (IS, FS)
+  pub fn new(IS: Vec<(usize, u64, u64)>, FS: Vec<(usize, u64, u64)>) -> Self {
+    ScanCircuit { IS, FS }
+  }
+}
+
+impl ScanCircuit {
+  /// Create a empty instance of [`ScanCircuit`]
+  pub fn empty(step_size: usize) -> Self {
+    ScanCircuit {
+      IS: vec![(0, 0, 0); step_size],
+      FS: vec![(0, 0, 0); step_size],
     }
   }
 }
